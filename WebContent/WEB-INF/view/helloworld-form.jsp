73670bc6699721676a2c4a9ed8fed1d4cd94bb66
kavros <akavroulakis@gmail.com>
2018-07-06 20:17:38 +0300

    First commit

---
+++ WebContent/WEB-INF/view/helloworld-form.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Hello World - Input form</title>
+</head>
+
+<body>
+	<form action="processFormVersionThree" method="GET">
+		
+		<input type="text" name="studentName"
+			placeholder="What is your name?">
+			
+		<input type="submit"/>
+	
+	</form>
+		
+</body>
+</html>
---
+++ src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,77 @@
+package com.luv2code.springdemo.mvc;
+
+import java.util.LinkedHashMap;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String country;
+	
+	private LinkedHashMap<String, String> favoriteLanguageOptions;
+	private String favoriteLanguage;
+
+	private String[] operatingSystems;
+
+	public Student() 
+	{
+		operatingSystems =new String[4];
+		 // populate favorite language options
+        favoriteLanguageOptions = new LinkedHashMap<>();
+        
+        // parameter order: value, display label
+        //
+        favoriteLanguageOptions.put("Java", "Java");
+        favoriteLanguageOptions.put("C#", "C#");
+        favoriteLanguageOptions.put("PHP", "PHP");
+        favoriteLanguageOptions.put("Ruby", "Ruby");        
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public LinkedHashMap<String, String> getFavoriteLanguageOptions() {
+		return favoriteLanguageOptions;
+	}
+
+	public void setFavoriteLanguageOptions(LinkedHashMap<String, String> favoriteLanguageOptions) {
+		this.favoriteLanguageOptions = favoriteLanguageOptions;
+	}
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] opertingSystems) {
+		this.operatingSystems = opertingSystems;
+	}
+	
+	
+}
